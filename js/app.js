a12346aee1ab2c7e7e94da268c49f41275846a9d
Laszlo Feher <feher.laszlo@gmail.com>
2018-07-19 13:28:04 +0200

    add images add.js functions to div element

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+/*
+* Hajók listájának megjelenítése
+* Ha így írjuk meg, akkor a függvény használatakor mutatja a magyarázatot
+* @param {Array} listSource: array a lista forrására
+*/
+function createListDiv(container) {
+  var listDiv = document.querySelector('.list-div');
+  // Van-e már div-a hajóknak? Ha nincs, akkor létrehozzuk.
+  if (!listDiv) {
+    listDiv = document.createElement('div');
+    listDiv.className = 'list-div';
+    container.appendChild(listDiv);
+  }
+  return listDiv;
+}
+
+function createSpaceship(listDiv, spaceship) {
+  var itemDiv = document.createElement('div');
+
+  var img = document.createElement('img');
+  img.src = '/img/' + spaceship.image;
+  img.alt = spaceship.model;
+
+  var span = document.createElement('span');
+  span.innerHTML = spaceship.model;
+
+  itemDiv.appendChild(img);
+  itemDiv.appendChild(span);
+
+  listDiv.appendChild(itemDiv);
+}
+
+function showSpaceshipList(listSource) {
+  var container = document.querySelector('.spaceship-list');
+  var listDiv = createListDiv(container);
+  for (let i = 0; i < listSource.length; i++) {
+    createSpaceship(listDiv, listSource[i]);
+  }
+}
+
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -11,7 +51,10 @@ function getData(url, callbackFunc) {
 
 function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
-  var userDatas = JSON.parse(xhttp.responseText);
+  // var userDatas = JSON.parse(xhttp.responseText);
+  // showSpaceshipList(userDatas[2].data);
+  var userDatas = JSON.parse(xhttp.responseText)[2].data;
+  showSpaceshipList(userDatas);
 }
 
 getData('/json/spaceships.json', successAjax);
